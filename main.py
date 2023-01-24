57fa7574c68012bc4a8e3ed294f434d07d04d47a
DomVidal <erickvidalemprego@gmail.com>
2023-01-24 19:10:33 -0300

    jogo commit

---
+++ main.py
@@ -0,0 +1,161 @@
+import time
+
+#historia
+print("Olá, você é o lorde dessa terra de neve, chamada reino de Victoria\n")
+time.sleep(6)
+print("Você perdeu suas memorias na nossa ultima batalha") 
+print("Mas a gente ganhou a guerra então isso compensa\n")
+time.sleep(8)
+print("Como você perdeu suas memorias eu vou resumir para você:")
+print("Basicamente o reino de VIctoria era seu por direito mas um outro lorde chamado Armadeus")
+print("Tramou contra o senhhor e usurpou o trono, então entramos em guerra e recentemente o derrotamos\n")
+time.sleep(11)
+print("Então você terá que fazer uma escolha, ou ficamos aqui cuidando do reino")
+print("Ou iremos reagrupar nossos homens")
+time.sleep(9)
+print("Eu recomendo fortemente a segunda opção pois se sofrermos um ataque seremos emgolidos\n")
+time.sleep(5)
+
+#escolhas
+escolha = int(input("1 = primeira opção e 2 = segunda opção, escolha:"))
+if escolha == 1:
+    print("Não é o que eu recomendo mais eu confio na sua sabedoria meu senhor\n")
+    time.sleep(3)
+    print("Vamos cuidar da economia do país")
+    time.sleep(3)
+    print("Nós estamos quebrados por causa da guerra entretanto nós temos o suficiente")
+    print("Para gerir o país sem usar muita luxuaria, claro\a")
+    time.sleep(5)
+    print("Mas eu já executei um plano: baixar impostos, procurar contrato com outros paises para desenvolvimento")
+    print("e usar os minerios de Victoria para nós desenvolver")
+    time.sleep(6)
+    print("Na fronteira de Victoria tem um minerio chamado enchi, ele é de otima qualidade")
+    print("Imagino que o senhor queira ir lá para verificar a área, então vou preparar cavalos")
+    time.sleep(7)
+    print("  A CAMINHO...  ")
+    print("""                                                           |>>>           
+                   _                      _                |             
+    ____________ .' '.    _____/----/-\ .' './========\   / \               
+   //// ////// /V_.-._\  |.-.-.|===| _ |-----| u    u |  /___\              
+  // /// // ///==\ u |.  || | ||===||||| |T| |   ||   | .| u |_ _ _ _ _ _   
+ ///////-\////====\==|:::::::::::::::::::::::::::::::::::|u u| U U U U U    
+ |----/\u |--|++++|..|'''''''''''::::::::::::::''''''''''|+++|+-+-+-+-+-+   
+ |u u|u | |u ||||||..|              '::::::::'           |===|>=== _ _ ==   
+ |===|  |u|==|++++|==|              .::::::::.           | T |....| V |..   
+ |u u|u | |u ||HH||         \|/    .::::::::::.                             
+ |===|_.|u|_.|+HH+|_              .::::::::::::.              _            
+                __(_)___         .::::::::::::::.         ___(_)__          
+---------------/  / \  /|       .:::::;;;:::;;:::.       |\  / \  \-------  
+______________/_______/ |      .::::::;;:::::;;:::.      | \_______\________
+|       |     [===  =] /|     .:::::;;;::::::;;;:::.     |\ [==  = ]   |    
+|_______|_____[ = == ]/ |    .:::::;;;:::::::;;;::::.    | \[ ===  ]___|____
+     |       |[  === ] /|   .:::::;;;::::::::;;;:::::.   |\ [=  ===] |      
+_____|_______|[== = =]/ |  .:::::;;;::::::::::;;;:::::.  | \[ ==  =]_|______
+ |       |    [ == = ] /| .::::::;;:::::::::::;;;::::::. |\ [== == ]      | 
+_|_______|____[=  == ]/ |.::::::;;:::::::::::::;;;::::::.| \[  === ]______|_
+   |       |  [ === =] /.::::::;;::::::::::::::;;;:::::::.\ [===  =]   |    
+___|_______|__[ == ==]/.::::::;;;:::::::::::::::;;;:::::::.\[=  == ]___|_____\n""")
+    time.sleep(8)
+    print("Estamos na fronteira senhor, aqui é um dos principais lugares do reino")
+    time.sleep(5)
+    print("o povo daqui foi um dos unicos que nós ajudou mesmo após nossa queda\n")
+    time.sleep(4)
+    print("Na verdade nós viemos aqui por um motivo mais importante que o enchi")
+    print("eu sou seu braço direito porém ainda falta o esquerdo e nós viemos buscar ele\n")
+    time.sleep(4)
+    print(" OLHA ELE VINDO LA...\n")
+    time.sleep(3)
+    print("""           _        _
+          /\\     ,'/|
+        _|  |\-'-'_/_/
+   __--'/`           \
+       /              \
+      /        "o.  |o"|
+      |              \/
+       \_          ___\
+         `--._`.   \;//
+              ;-.___,'
+             /
+           ,'
+        _-'""")
+    time.sleep(4)
+    print("esse é o Fantasma, seu lobo e seu braço direito")
+    time.sleep(4)
+    print("agora, sim o trio está formado e nós podemos continuar")
+
+
+elif escolha == 2:
+    print("Essa é a escolha certa, eu sou NORD seu braço direito então para começar")
+    time.sleep(5)
+    print("Vamos atrás do seu braço esquerdo, o seu lobo, o FANTASMA\n")
+    time.sleep(4)
+    print("""           _        _
+          /\\     ,'/|
+        _|  |\-'-'_/_/
+   __--'/`           \
+       /              \
+      /        "o.  |o"|
+      |              \/
+       \_          ___\
+         `--._`.   \;//
+              ;-.___,'
+             /
+           ,'
+        _-'""")
+    time.sleep(4)
+    print("agora, sim o trio está formado e nós podemos continuar\n")
+    time.sleep(4)
+
+
+#historia
+print("Vamos pegar um pouco de Enchi, um dos minerios mais valiosos do reino")
+print("depois vamos a floresta caçar alguns monstros e fazer algum dinheiro\n")
+time.sleep(5)
+print(" COLETANDO ENCHI...")
+time.sleep(5)
+print("Bom, já temos o suficiente eu imagino, vamos deixar o FANTASMA caçar alguns monstros\n")
+time.sleep(4)
+print("""          /           /   
+         /' .,,,,  ./       
+        /';'     ,/        
+       / /   ,,//,`'`     
+      ( ,, '_,  ,,,' ``   
+      |    /@  ,,, ;" `   
+     /    .   ,''/' `,``   
+    /   .     ./, `,, ` ; 
+ ,./  .   ,-,',` ,,/''\,' 
+|   /; ./,,'`,,'' |   |   
+|     /   ','    /    |   
+ \___/'   '     |     |  
+  `,,'   |      /     `\  
+        /      |        ~\  
+       '       (          
+      :                    
+     ; .         \         
+    :   \         ;       """)
+time.sleep(4)
+print("Otimo, ali tem um pequeno dragão, vá atacar Fantasma\n")
+time.sleep(3)
+
+#combate
+monstro = 20
+ataque1 = 20
+ataque2 = 29
+while monstro == 20:
+    print("Senhor, de ordens de ataque")
+    time.sleep(2)
+    print("1 = Mordida     2 = Aranhão\n")
+    time.sleep(5)
+    decisaõ2 = int(input("Por favor, escolha 1 ou 2 meu senhor: "))
+    if decisaõ2 == 1:
+        print("Você ordenou que o Fantasma mordesse o Dragão")
+        resultado = (monstro - ataque1)
+        print("Resultado:", resultado)
+    elif decisaõ2 == 2:
+        print("Você ordenou que o Fantasma arranha-se o Dragão")
+        resultado2 = (monstro - ataque2)
+    monstro <= 0
+    print("Você matou o dragão e zerou a beta :)")
+    break
+    
+
